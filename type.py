d2d700b9b2ca2c8b033951f6e87888dd7f359bf7
Thomas P <thomaspj2003@gmail.com>
2023-06-26 19:17:08 +0200

    Improve type safety of the type mapping dict.

--- type.py
+++ type.py
@@ -1,25 +1,25 @@
 from enum import Enum
 from language import Language
 
-TYPE_MAPPING = {
-    "string": {
-        "python": "str",
-        "elm": "String"
-    },
-    "int": {
-        "python": "int",
-        "elm": "Int"
-    },
-    "float": {
-        "python": "float",
-        "elm": "Float"
-    },
-}
-
 class Type(Enum):
     STRING = "string"
     INT = "int"
     FLOAT = "float"
 
+TYPE_MAPPING: dict[Type, dict[Language, str]] = {
+    Type.STRING: {
+        Language.PYTHON: "str",
+        Language.ELM: "String"
+    },
+    Type.INT: {
+        Language.PYTHON: "int",
+        Language.ELM: "Int"
+    },
+    Type.FLOAT: {
+        Language.PYTHON: "float",
+        Language.ELM: "Float"
+    },
+}
+
 def get_type(language: Language, type: Type):
-    return TYPE_MAPPING[type.value][language.value]
+    return TYPE_MAPPING[type][language]
