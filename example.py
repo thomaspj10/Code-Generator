0ecebfaa6ff4742c033f847bb8da575e006c74c4
Thomas P <thomaspj2003@gmail.com>
2023-07-13 13:19:42 +0200

    Update the example

--- example.py
+++ example.py
@@ -2,22 +2,22 @@ from builders import class_builder, file_builder
 from language import Language
 from type import Type
 
-address_cls = (
+address = (
     class_builder()
     .name("Address")
     .attribute("street", Type.STRING)
     .attribute("number", Type.INT)
 )
 
-person_cls = (
+person = (
     class_builder()
     .name("Person")
     .attribute("name", Type.STRING)
     .attribute("age", Type.INT)
-    .attribute("address", address_cls)
+    .attribute("address", address)
 )
 
-builder = file_builder().add(person_cls)
+builder = file_builder().add(person)
 
 builder.save("Types.elm", "./elm/src/", Language.ELM)
 builder.save("types.py", "./python/", Language.PYTHON)
