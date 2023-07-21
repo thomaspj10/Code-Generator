4d633512585e570dc392357eaeb728a84cef61cd
Thomas P <thomaspj2003@gmail.com>
2023-07-21 17:25:53 +0200

    Finish encoders and decoders generation.

--- language.py
+++ language.py
@@ -1,6 +1,18 @@
+from typing import Callable
 from enum import Enum
 
 class Language(Enum):
     PYTHON = "python"
     ELM = "elm"
-    ELM_DECODE = "elm_decode"
+    ELM_DECODERS = "elm_decoders"
+    ELM_ENCODERS = "elm_encoders"
+
+CLASS_NAME_MAPPING: dict[Language, Callable[[str], str]] = {
+    Language.PYTHON: lambda name: name,
+    Language.ELM: lambda name: name,
+    Language.ELM_DECODERS: lambda name: name[:1].lower() + name[1:] + "Decoder",
+    Language.ELM_ENCODERS: lambda name: name[:1].lower() + name[1:] + "Encoder",
+}
+
+def format_class_name(name: str, Language: Language) -> str:
+    return CLASS_NAME_MAPPING[Language](name)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from enum import Enum
 from dataclasses import dataclass
-from language import Language
+from language import Language, format_class_name
 
 class Type(Enum):
     INT = "int"
@@ -23,27 +23,32 @@ TYPE_MAPPING: dict[Type, dict[Language, str]] = {
     Type.STRING: {
         Language.PYTHON: "str",
         Language.ELM: "String",
-        Language.ELM_DECODE: "JD.string",
+        Language.ELM_DECODERS: "JD.string",
+        Language.ELM_ENCODERS: "JE.string",
     },
     Type.INT: {
         Language.PYTHON: "int",
         Language.ELM: "Int",
-        Language.ELM_DECODE: "JD.int",
+        Language.ELM_DECODERS: "JD.int",
+        Language.ELM_ENCODERS: "JE.int",
     },
     Type.CLASS: {
         Language.PYTHON: "{0}",
         Language.ELM: "{0}",
-        Language.ELM_DECODE: "{0}Decoder",
+        Language.ELM_DECODERS: "{0}",
+        Language.ELM_ENCODERS: "{0}",
     },
     Type.LIST: {
         Language.PYTHON: "list[{0}]",
         Language.ELM: "List ({0})",
-        Language.ELM_DECODE: "JD.list ({0})",
+        Language.ELM_DECODERS: "JD.list ({0})",
+        Language.ELM_ENCODERS: "JE.list ({0})",
     },
     Type.MAYBE: {
         Language.PYTHON: "{0} | None",
         Language.ELM: "Maybe ({0})",
-        Language.ELM_DECODE: "JD.maybe ({0})",
+        Language.ELM_DECODERS: "JD.maybe ({0})",
+        Language.ELM_ENCODERS: "{0}",
     },
 }
 
@@ -99,12 +104,85 @@ def get_type(language: Language, type: Type, generic_arguments: list[str]):
 
 def type_to_code(custom_type: CustomType, language: Language) -> str:
     if isinstance(custom_type.type, str):
-        return custom_type.type
+        return format_class_name(custom_type.type, language)
 
     if isinstance(custom_type.type, Type):
         return get_type(language, custom_type.type, [type_to_code(argument, language) for argument in custom_type.generic_arguments])
 
     return type_to_code(custom_type.type, language)
 
-print(type_to_code(type, Language.ELM_DECODE))
-print(type_to_code(type2, Language.ELM_DECODE))
+def generate_python(dtos: list[CustomDto]) -> str:
+    INDENT = " " * 4
+    result = ""
+    
+    result += "from annotations import __future__\n"
+    result += "from dataclasses import dataclass\n"
+
+    for dto in dtos:
+        result += "\n@dataclass\n"
+        result += f"class {dto.name}\n"
+        for attribute in dto.attributes:
+            result += f"{INDENT}{attribute.name}: {type_to_code(attribute.type, Language.PYTHON)}\n"
+        
+
+    return result
+
+def generate_elm(dtos: list[CustomDto]) -> str:
+    INDENT = " " * 4
+    result = ""
+
+    result += "import Json.Decode as JD\n"
+    result += "import Json.Encode as JE\n"
+    result += "import Json.Decode.Pipeline as JDP\n"
+
+    # Generate the models
+    for dto in dtos:
+        result += f"\ntype alias {dto.name} =\n"
+
+        for index, attribute in enumerate(dto.attributes):
+            char = "{" if index == 0 else ","
+            result += f"{INDENT}{char} {attribute.name} : {type_to_code(attribute.type, Language.ELM)}\n"
+        
+        result += INDENT + "}\n"
+
+    result += generate_elm_decoders(dtos)
+    result += generate_elm_encoders(dtos)
+
+    return result
+
+
+def generate_elm_decoders(dtos: list[CustomDto]) -> str:
+    INDENT = " " * 4
+    result = ""
+    
+    for dto in dtos:
+        decoder_name = format_class_name(dto.name, Language.ELM_DECODERS)
+
+        result += f"\n{decoder_name} : JD.Decoder {dto.name}\n"
+        result += f"{decoder_name} =\n"
+        result += f"{INDENT}JD.succeed {dto.name}\n"
+
+        for attribute in dto.attributes:
+            result += f"{INDENT * 2}|> JDP.required \"{attribute.name}\" {type_to_code(attribute.type, Language.ELM_DECODERS)}\n"
+
+    return result
+
+def generate_elm_encoders(dtos: list[CustomDto]) -> str:
+    INDENT = " " * 4
+    result = ""
+    
+    for dto in dtos:
+        encoder_name = format_class_name(dto.name, Language.ELM_ENCODERS)
+
+        result += f"\n{encoder_name} : {dto.name} -> JE.Value\n"
+        result += f"{encoder_name} object =\n"
+        result += f"{INDENT}JE.object\n"
+
+        for index, attribute in enumerate(dto.attributes):
+            char = "[" if index == 0 else ","
+            result += f"{INDENT * 2}{char} ( \"{attribute.name}\", {type_to_code(attribute.type, Language.ELM_ENCODERS)} object.{attribute.name} )\n"
+        result += INDENT * 2 + "]\n"
+
+    return result
+
+print(generate_elm([address, person]))
